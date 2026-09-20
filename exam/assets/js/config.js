27412dfe3c38c977275ddacf6097baec198e6a38
APMatHS <153115073+APMatHS@users.noreply.github.com>
2026-09-20 14:10:32 +0700

    exam: add application configuration

---
+++ exam/assets/js/config.js
@@ -0,0 +1,18 @@
+export const CONFIG = Object.freeze({
+  appName:'AI-CLO EXAM',
+  supabaseUrl:'https://unpqmttdkmmbnmmcfqbj.supabase.co',
+  supabasePublishableKey:'sb_publishable_wHA6FYHcS47A5dC9iWfvuw_KBZsQBJk',
+  supabaseJsVersion:'2.116.0',
+  storageBucket:'exam-files',
+  studentFunction:'student-exam',
+  assetFunction:'exam-assets',
+  heartbeatMs:15000,
+  liveRefreshMs:5000,
+  autosaveDebounceMs:350,
+  defaultRetentionDays:30,
+  allowedStaffRoles:['admin','exam_officer','teacher','proctor'],
+  permissions:[
+    'manage_exam','manage_members','manage_sessions','manage_roster','manage_paper',
+    'manage_live','view_results','export_results','view_correct_answers','manage_assets'
+  ]
+});
