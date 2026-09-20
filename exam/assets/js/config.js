f26061b02ba326b407b8d52af98849333b41e8fa
APMatHS <153115073+APMatHS@users.noreply.github.com>
2026-09-20 14:29:07 +0700

    exam: add explicit view exam permission

--- exam/assets/js/config.js
+++ exam/assets/js/config.js
@@ -5,14 +5,13 @@ export const CONFIG = Object.freeze({
   supabaseJsVersion:'2.116.0',
   storageBucket:'exam-files',
   studentFunction:'student-exam',
-  assetFunction:'exam-assets',
   heartbeatMs:15000,
   liveRefreshMs:5000,
   autosaveDebounceMs:350,
   defaultRetentionDays:30,
   allowedStaffRoles:['admin','exam_officer','teacher','proctor'],
   permissions:[
-    'manage_exam','manage_members','manage_sessions','manage_roster','manage_paper',
+    'view_exam','manage_exam','manage_members','manage_sessions','manage_roster','manage_paper',
     'manage_live','view_results','export_results','view_correct_answers','manage_assets'
   ]
 });
